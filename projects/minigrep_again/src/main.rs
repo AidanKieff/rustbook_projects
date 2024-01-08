0ebfc35837d563548e29400d97487b5b279eb643
Aidan K <ak@umorpha.io>
2024-01-08 16:03:00 -0700

    added use of config struct to parse function

--- projects/minigrep_again/src/main.rs
+++ projects/minigrep_again/src/main.rs
@@ -5,13 +5,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
 
-    println!("searching for {}", query);
-    println!("In file {}", file_path);
+    println!("searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("should have been able to read the file");
 
     println!("With text: \n{contents}");
@@ -24,10 +24,10 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
 
-    (query, file_path)
+    Config {query, file_path}
 }
 
