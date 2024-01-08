70aa0f262f3cf0ff0dda651e543deb8f4dc0a789
Aidan K <ak@umorpha.io>
2024-01-08 16:33:36 -0700

    test commit

--- projects/minigrep_again/src/main.rs
+++ projects/minigrep_again/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     dbg!(&args);
 
     let config = parse_config(&args);
-
+    //just a test comment
 
     println!("searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -27,6 +27,7 @@ struct Config {
 fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
     let file_path = args[2].clone();
+
 
     Config {query, file_path}
 }
