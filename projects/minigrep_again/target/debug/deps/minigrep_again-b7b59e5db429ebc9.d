3536f7a4af143ea09219ee90b9c461c615d13b3f
Aidan K <ak@umorpha.io>
2024-01-08 10:30:33 -0700

    start of minigrep_again project.
    added initial vector to collect environment input, debuging to confirm this works.
    this is in path: rustbook/projects/minigrep_again/

---
+++ projects/minigrep_again/target/debug/deps/minigrep_again-b7b59e5db429ebc9.d
@@ -0,0 +1,5 @@
+/home/a/Documents/Rust/rustbook/projects/minigrep_again/target/debug/deps/minigrep_again-b7b59e5db429ebc9.rmeta: src/main.rs
+
+/home/a/Documents/Rust/rustbook/projects/minigrep_again/target/debug/deps/minigrep_again-b7b59e5db429ebc9.d: src/main.rs
+
+src/main.rs:
